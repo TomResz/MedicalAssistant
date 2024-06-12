cd10454b4c7fe0ca7aaa3a85bcd518493f017e03
TomResz <tomaszres@interia.pl>
2024-06-12 10:10:44 +0200

    Changed quart to hangfire

---
+++ MedicalAssist.Infrastructure/BackgroundJobs/BackgroundJobExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MedicalAssist.Infrastructure.BackgroundJobs;
+public static class BackgroundJobExtensions
+{
+    public static IApplicationBuilder UseOutboxMessageProcessing(this WebApplication app)
+    {
+        app.Services
+            .GetRequiredService<IRecurringJobManager>()
+            .AddOrUpdate<IProcessOutboxMessagesJob>(
+            "outbox-processing", j => j.ExecuteJob(default),
+            "0/15 * * * * *");
+        return app;
+    }
+}
--- MedicalAssist.Infrastructure/BackgroundJobs/Extensions.cs
+++ MedicalAssist.Infrastructure/BackgroundJobs/Extensions.cs
@@ -1,22 +1,46 @@
-﻿using MedicalAssist.Infrastructure.Outbox;
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using HangfireBasicAuthenticationFilter;
+using MedicalAssist.Infrastructure.BackgroundJobs;
+using MedicalAssist.Infrastructure.Outbox;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Quartz;
 
 namespace MedicalAssist.Infrastructure.BackgrounJobs;
-internal static class Extensions
+public static class Extensions
 {
-	internal static IServiceCollection AddBackgroundJobs(this IServiceCollection services)
-		=> services.AddQuartz(conf =>
-		{
-			var jobKey = JobKey.Create(nameof(ProcessOutboxMessagesJob));
-			conf
-				.AddJob<ProcessOutboxMessagesJob>(jobKey)
-				.AddTrigger(
-				trigger =>
-					trigger.ForJob(jobKey).WithSimpleSchedule(
-						schedule => schedule.WithIntervalInSeconds(15).RepeatForever())
-			);
-			conf.UseMicrosoftDependencyInjectionJobFactory();
-		})
-		.AddQuartzHostedService(settings => settings.WaitForJobsToComplete = true);
+    private const string databaseConnectionSection = "postgres:connectionString";
+    private const string hangfireUserSection = "Hangfire:User";
+    private const string hangfirePasswordSection = "Hangfire:Password";
+
+    internal static IServiceCollection AddBackgroundJobs(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHangfire(cfg =>
+        {
+            cfg.UsePostgreSqlStorage(opt =>
+                opt.UseNpgsqlConnection(configuration[databaseConnectionSection] ?? throw new ArgumentNullException()));
+        });
+
+        services.AddHangfireServer(opt => opt.SchedulePollingInterval = TimeSpan.FromSeconds(5));
+
+        services.AddScoped<IProcessOutboxMessagesJob, ProcessOutboxMessagesJob>();
+        return services;
+    }
+
+    public static IApplicationBuilder UseHangfireDashboard(this IApplicationBuilder app, IConfiguration configuration)
+    {
+        app.UseHangfireDashboard("/hangfire", options: new DashboardOptions
+        {
+            Authorization = new[]
+            {
+                new HangfireCustomBasicAuthenticationFilter
+                {
+                    User = configuration[hangfireUserSection] ?? throw new ArgumentNullException(hangfireUserSection),
+                    Pass = configuration[hangfirePasswordSection] ?? throw new ArgumentNullException(hangfirePasswordSection)
+                }
+            },
+        });
+        return app;
+    }
 }
